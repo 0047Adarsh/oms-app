1dafcb2a1b0b9daea4d8bc87aa63ff1a2a39480a
Adarsh A Devadiga <adarsh9742007@gmail.com>
2025-05-24 01:25:42 +0530

    Resolved the Order Route

--- oms-api/src/db/supabaseClient.js
+++ oms-api/src/db/supabaseClient.js
@@ -1,5 +1,8 @@
 // db/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
+import doteenv from 'dotenv';
+
+doteenv.config()
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
